fa2f561bd1d6883c92d1dfc1d3e476ac57b653d0
Al Amin Chowdhury <alonealamin109@gmail.com>
2026-09-22 20:56:10 +0600

    inupt

--- 1st_chapter/frist.py
+++ 1st_chapter/frist.py
@@ -1,14 +1,2 @@
-print("Hello to me, It's just me and me")
-print("Let's do that this time")
-print(
-
-" date is 21 sep 2026 I'll push code every single day " \
-"I'll write code every single day" \
-"I'll learn python every single day"
-
-
-)
-
-print("let's do it, It's now or never")
-
-name = input("What's you'r name? ")
+input("What's you'r name ? ")
+print("Hello, world")
